774bc0fcdeae1d617f08c6f6fa104e37b4a4e1dc
Quynh Hoang <htnquynh@users.noreply.github.com>
2022-01-05 02:21:25 +0700

    update server base

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
   server: {
+    origin: 'https://shopfreshapi.herokuapp.com/',
     proxy: {
-      "/product": {
+      "/products": {
         target: "https://shopfreshapi.herokuapp.com",
         changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/product/, ""),
       },
       "/group": {
         target: "https://shopfreshapi.herokuapp.com",
         changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/group/, ""),
       },
     },
   },
-  plugins: [vue()],
 })
